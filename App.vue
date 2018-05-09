6181f4e4678821ce55a1a6f66c7b4de95d391610
PC-20160528ZMPM\Administrator <dkxhy1010@126.com>
2018-05-09 18:05:29 +0800

    基本结构搭建

---
+++ App.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<h1>hellow Vue</h1>
+	</div>
+</template>
+<script>
+	export default {
+		return {}
+	}
+</script>
+<style>
+	
+</style>
---
+++ main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import App from "./App.vue"
+var vm = new Vue({
+	render: h=>h(App)
+})
